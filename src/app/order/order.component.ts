4b399761bb3b6667a7c98a3cd3580c01f5ff29e5
henriquesd <hsd.foz@gmail.com>
2019-07-02 16:43:05 -0300

    Changed order page from template form to reactive form

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+
 import { RadioOption } from '../shared/radio/radio-option.model';
 import { OrderService } from './order.service';
 import { CartItem } from '../restaurant-detail/shopping-cart/cart-item.model';
@@ -11,6 +13,7 @@ import { Router } from '@angular/router';
 })
 export class OrderComponent implements OnInit {
 
+  orderForm: FormGroup;
   delivery: number = 6;
 
   paymentOptions: RadioOption[] = [
@@ -21,9 +24,19 @@ export class OrderComponent implements OnInit {
   ]
 
   constructor(private OrderService: OrderService, 
-                private router: Router) { }
+                private router: Router,
+                private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.orderForm = this.formBuilder.group({
+      name: '',
+      email: this.formBuilder.control(''),
+      emailConfirmation: this.formBuilder.control(''),
+      address: this.formBuilder.control(''),
+      number: this.formBuilder.control(''),
+      optionalAddress: this.formBuilder.control(''),
+      paymentOption: this.formBuilder.control('')
+    });
   }
 
   itemsValue(): number{
